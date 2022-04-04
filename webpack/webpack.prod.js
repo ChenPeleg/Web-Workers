e55b430b092a14d6064a170852a8036566fcc94f
chenp <chenpeleg@gmail.com>
2022-04-04 05:24:17 +0300

    added gh-pages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,16 @@
 // @ts-nocheck
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const path = require('path');
 module.exports = merge(common, {
     mode: 'production',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: './'
+    entry: "./src/app.js",
+    output: {
+
+        filename: "main.[contenthash].js",
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: './'
+
+    },
+
 });
